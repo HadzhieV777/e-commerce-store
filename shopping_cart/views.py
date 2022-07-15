f069c4aaa506461a0e99b42b49b2fdd05807cd9d
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-07-16 00:19:14 +0300

    Edited views.py

--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -21,3 +21,16 @@ def add_to_cart(request):
         
         response = JsonResponse({'qty': product_qty, 'price': product_price})
         return response
+
+
+def cart_update(request):
+    basket = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        product_qty = int(request.POST.get('productqty'))
+        basket.update(product=product_id, qty=product_qty)
+
+        basketqty = basket.__len__()
+        baskettotal = basket.get_total_price()
+        response = JsonResponse({'qty': basketqty, 'subtotal': baskettotal})
+        return response
