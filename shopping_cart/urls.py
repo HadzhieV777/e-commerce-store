b259d37c3d6e8e5242cffc1f2c5c2d56d049cdd1
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-07-16 00:18:57 +0300

    Edited urls.py

--- shopping_cart/urls.py
+++ shopping_cart/urls.py
@@ -1,14 +1,10 @@
 from django.urls import path
-from shopping_cart.views import cart_summary, add_to_cart
+from shopping_cart.views import cart_summary, add_to_cart, cart_update
 
 app_name = 'cart'
 
 urlpatterns = (
     path('', cart_summary, name='cart summary'),
     path('add/', add_to_cart, name='add to cart'),
+    path('update/', cart_update, name='update cart'),
 )
-
-
-
-
-
